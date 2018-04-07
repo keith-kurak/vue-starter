c40365cbfddd1452757b7c30565542729ab4f17b
Keith Kurak <keith.kurak@gmail.com>
2018-04-07 17:44:31 -0400

    initial check in

---
+++ src/components/Page2.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <router-link to="/">Go back</router-link>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Page2',
+  data() {
+    return {
+      msg: 'This is page 2',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
